970ed065215c43277764d5aeb3e30a375a268786
yashim-deriv <yashim@regentmarkets.com>
2024-01-26 11:49:40 +0800

    feat: add scaffolding for websocket

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
-import { useState } from "react";
+import { useEffect, useRef } from "react";
 
 function App() {
-    const [count, setCount] = useState(0);
+    const websocket = useRef<WebSocket>();
+
+    useEffect(() => {
+        if (!websocket?.current && ![0, 1].includes(websocket?.current?.readyState || 3)) {
+            websocket.current = new WebSocket("wss://ws.derivws.com/websockets/v3?app_id=1089");
+        }
+        websocket.current?.addEventListener("open", () => {
+            if (websocket.current?.readyState === 1) {
+                websocket.current.send(
+                    JSON.stringify({
+                        ping: 1,
+                    })
+                );
+            }
+        });
+        websocket?.current?.addEventListener("message", (response) => {
+            console.log(JSON.parse(response.data));
+        });
+
+        return () => {
+            if ([0, 1].includes(websocket?.current?.readyState || 3)) {
+                websocket?.current?.close();
+            }
+        };
+    }, []);
 
     return (
         <>
             <div>Interview</div>
-            <button onClick={() => setCount((prev) => prev + 1)}>{count}</button>
+            <div>Solution goes here.</div>
         </>
     );
 }
